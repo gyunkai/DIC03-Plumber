ddd6b3b6b4edbfea0e554fc6f34d16d533dc5259
Bob-wentao <143519535+Bob-wentao@users.noreply.github.com>
2025-03-02 20:33:45 +0800

    database-release1

--- app/SignupPage/page.jsx
+++ app/SignupPage/page.jsx
@@ -1,13 +1,53 @@
 "use client";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 const SignUpPage = () => {
   const router = useRouter();
 
-  const handleContinue = () => {
-    // Adjust this route to whatever page you want to go after signup
-    router.push("/ChatPage");
+  // Use state to store form data
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleContinue = async (e) => {
+    e.preventDefault();
+
+    // Simple validation: Check if password and confirm password match
+    if (formData.password !== formData.confirmPassword) {
+      alert("Password and Confirm Password do not match");
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: formData.name,
+          email: formData.email,
+          password: formData.password,
+        }),
+      });
+
+      if (res.ok) {
+        // Redirect to the designated page after successful registration
+        router.push("/ChatPage");
+      } else {
+        const errorData = await res.json();
+        alert(errorData.error || "Registration failed");
+      }
+    } catch (error) {
+      console.error("Submission error", error);
+      alert("An error occurred during registration");
+    }
   };
 
   return (
@@ -20,7 +60,6 @@ const SignUpPage = () => {
           alt="NYU logo"
           className="absolute top-4 left-4 w-40 h-auto object-contain"
         />
-
         {/* Main illustration */}
         <img
           src="/image/delivery-rafiki-2.png"
@@ -36,12 +75,15 @@ const SignUpPage = () => {
           Become a member and enjoy your personal tutor for Computer Science & Data Science.
         </p>
 
-        <form className="flex flex-col gap-4 mt-6">
+        <form className="flex flex-col gap-4 mt-6" onSubmit={handleContinue}>
           {/* Full Name */}
           <label className="flex flex-col">
             <span className="text-gray-600 font-medium">Full Name</span>
             <input
               type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
               className="mt-1 p-2 border border-gray-400 rounded-md"
               placeholder="Enter your full name"
             />
@@ -52,6 +94,9 @@ const SignUpPage = () => {
             <span className="text-gray-600 font-medium">Email Address</span>
             <input
               type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
               className="mt-1 p-2 border border-gray-400 rounded-md"
               placeholder="Enter your email"
             />
@@ -62,6 +107,9 @@ const SignUpPage = () => {
             <span className="text-gray-600 font-medium">Password</span>
             <input
               type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
               className="mt-1 p-2 border border-gray-400 rounded-md"
               placeholder="Enter a password"
             />
@@ -72,6 +120,9 @@ const SignUpPage = () => {
             <span className="text-gray-600 font-medium">Confirm Password</span>
             <input
               type="password"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
               className="mt-1 p-2 border border-gray-400 rounded-md"
               placeholder="Re-enter your password"
             />
@@ -79,8 +130,7 @@ const SignUpPage = () => {
 
           {/* Continue Button */}
           <button
-            type="button"
-            onClick={handleContinue}
+            type="submit"
             className="bg-[#c6c7f8] text-white font-medium rounded-md h-12 w-full"
           >
             Continue
---
+++ prisma/migrations/20250226120932_try1/migration.sql
@@ -0,0 +1,134 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "oauthId" TEXT NOT NULL,
+    "oauth" BOOLEAN NOT NULL,
+    "consent" BOOLEAN,
+    "systemRole" TEXT NOT NULL DEFAULT 'user',
+    "researchNotes" JSONB,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserSession" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "sessionStartTime" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessionEndTime" TIMESTAMP(3),
+    "researchNotesForSession" JSONB,
+
+    CONSTRAINT "UserSession_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserInteraction" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "sessionId" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "actionName" TEXT NOT NULL DEFAULT '',
+    "actions" JSONB[],
+
+    CONSTRAINT "UserInteraction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Class" (
+    "id" TEXT NOT NULL,
+    "classMetadata" JSONB NOT NULL,
+    "createdDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "professorId" TEXT NOT NULL,
+
+    CONSTRAINT "Class_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Enrollment" (
+    "classId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "Enrollment_pkey" PRIMARY KEY ("classId","userId")
+);
+
+-- CreateTable
+CREATE TABLE "PendingEnrollment" (
+    "id" TEXT NOT NULL,
+    "classId" TEXT NOT NULL,
+    "userEmail" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "PendingEnrollment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ClassMaterial" (
+    "id" TEXT NOT NULL,
+    "classId" TEXT NOT NULL,
+    "materialType" TEXT NOT NULL,
+    "materialMetadata" JSONB NOT NULL,
+    "materialContent" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "uploadedDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "availableDate" TIMESTAMP(3) NOT NULL DEFAULT '1970-01-01 00:00:00 +00:00',
+
+    CONSTRAINT "ClassMaterial_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Document" (
+    "id" TEXT NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "fileUrl" TEXT NOT NULL,
+    "chunks" JSONB,
+    "uploadedDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "classMaterialId" TEXT,
+
+    CONSTRAINT "Document_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_oauthId_key" ON "User"("oauthId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- AddForeignKey
+ALTER TABLE "UserSession" ADD CONSTRAINT "UserSession_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserInteraction" ADD CONSTRAINT "UserInteraction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserInteraction" ADD CONSTRAINT "UserInteraction_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "UserSession"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Enrollment" ADD CONSTRAINT "Enrollment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PendingEnrollment" ADD CONSTRAINT "PendingEnrollment_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ClassMaterial" ADD CONSTRAINT "ClassMaterial_classId_fkey" FOREIGN KEY ("classId") REFERENCES "Class"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Document" ADD CONSTRAINT "Document_classMaterialId_fkey" FOREIGN KEY ("classMaterialId") REFERENCES "ClassMaterial"("id") ON DELETE SET NULL ON UPDATE CASCADE;
