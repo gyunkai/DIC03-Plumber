eea6d462e55a65edd5de958dd51bc9cab5f44e4f
Kenny <kenny.su@nyu.edu>
2025-03-10 20:54:41 +0800

    clean unnecessary packages

---
+++ app/api/signup/route.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '../../lib/prisma';
+import bcrypt from 'bcryptjs';
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
+    const { name, email, password } = body;
+
+    if (!name || !email || !password) {
+      return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json({ error: 'Email already exists' }, { status: 400 });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        oauth: false,
+
+      },
+    });
+
+    return NextResponse.json({ message: 'User registered successfully', user: newUser }, { status: 201 });
+  } catch (error) {
+    console.error('Signup error:', error);
+    return NextResponse.json({ error: 'Registration failed, please try again later' }, { status: 500 });
+  }
+};
